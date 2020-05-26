dc2c19a5e91c775bd8796c1db1f189be9746e9fb
Jason Mo <jasonmo0332@gmail.com>
2020-05-26 02:43:19 -0700

    Initial Commit

---
+++ FoodMood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodMood
+//
+//  Created by Jason Mo on 5/26/20.
+//  Copyright © 2020 Jason Mo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
